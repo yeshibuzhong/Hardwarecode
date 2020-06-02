dca25837b7232dc70a7b767e0ef00fc4382a7c0d
a83061601 <a83061601@gmail.com>
2020-06-02 22:24:42 +0800

    feat:修改错误，可以直接运行
    Signed-off-by: a83061601 <a83061601@gmail.com>

--- app/src/main/java/com/lixin/hardwarecode/Mainhook.java
+++ app/src/main/java/com/lixin/hardwarecode/Mainhook.java
@@ -24,11 +24,11 @@ public class Mainhook implements IXposedHookLoadPackage {
 
             new Hook().HookTest(loadPackageParam); // 动态生效 不用重启
             new RootCloak().handleLoadPackage(loadPackageParam);
-            new XBuild(loadPackageParam);  //build
-            new Phone(loadPackageParam);  // TelephonyManager
-            new Resolution().Display(loadPackageParam);  // 屏幕
-            new OpenGL().OpenGLTest(loadPackageParam);   // 显卡
-            new Cpuinfo(loadPackageParam);         // CPU*/
+//            new XBuild(loadPackageParam);  //build
+//            new Phone(loadPackageParam);  // TelephonyManager
+//            new Resolution().Display(loadPackageParam);  // 屏幕
+//            new OpenGL().OpenGLTest(loadPackageParam);   // 显卡
+//            new Cpuinfo(loadPackageParam);         // CPU*/
 
 
 
