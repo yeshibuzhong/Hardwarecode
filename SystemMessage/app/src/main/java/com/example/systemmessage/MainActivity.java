8abce2d04ed5dc0d0a5eeab12ec0f37ec74fcf57
yahui <83061601@qq.com>
2020-06-09 10:09:10 +0800

    feat:更新代码
    Signed-off-by: yahui <83061601@qq.com>

--- SystemMessage/app/src/main/java/com/example/systemmessage/MainActivity.java
+++ SystemMessage/app/src/main/java/com/example/systemmessage/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.systemmessage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageInfo;
@@ -18,33 +21,63 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import java.lang.reflect.Method;
+import java.net.NetworkInterface;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<permissionCheck> extends AppCompatActivity {
+
+    private static class PhoneMessage
+    {
+        String imei;
+        String phoneNumber;
+        String subscriberId;
+        String simOperator;
+        String simCountryIso;
+        String simSerialNumber;
+        String simState;
+        String simOperatorName;
+    }
+
 
     String TAG = "MainActivity";
     private ListView listview;
+    private static final String TODO = null;
+    final int MY_PERMIEAD_CONTACTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.mylistview);
-        listview = (ListView)findViewById(R.id.mylistview);
+        TelephonyManager Phone = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        //获取权限状态
+        int result =
+                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE);
+        //判断权限是否开启
+        if (result != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE}, MY_PERMIEAD_CONTACTS);
+        } else {
+            setContentView(R.layout.mylistview);
+            listview = (ListView)findViewById(R.id.mylistview);
 
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this,putData(),R.layout.list_item,
-                new String[]{"name","age"},new int[]{R.id.title,R.id.value});
-        listview.setAdapter(simpleAdapter);
+            SimpleAdapter simpleAdapter = new SimpleAdapter(this,putData(),R.layout.list_item,
+                    new String[]{"name","age"},new int[]{R.id.title,R.id.value});
+            listview.setAdapter(simpleAdapter);
+
+        }
 
     }
 
     public List<Map<String,Object>> putData(){
 
         List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+
+        PhoneMessage message = getPhoneMessage(this);
 
         list.add(createMap(new String[]{"Build Manufacturer", android.os.Build.MANUFACTURER}));
         list.add(createMap(new String[]{"Build Product Name", android.os.Build.DEVICE}));
@@ -57,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         list.add(createMap(new String[]{"Build Version Name", getAppVersionName(this)}));
         list.add(createMap(new String[]{"Build Sdk api", Build.VERSION.SDK}));
         list.add(createMap(new String[]{"Version CodeName", android.os.Build.VERSION.CODENAME}));
-        list.add(createMap(new String[]{"Build c", ""}));
+        list.add(createMap(new String[]{"Build Host", ""}));
         list.add(createMap(new String[]{"Version incremental", android.os.Build.VERSION.INCREMENTAL}));
         list.add(createMap(new String[]{"Build date", ""}));
         list.add(createMap(new String[]{"Display id", Build.DISPLAY}));
@@ -68,14 +101,14 @@ public class MainActivity extends AppCompatActivity {
         list.add(createMap(new String[]{"Build User Agent", Build.USER}));
         list.add(createMap(new String[]{"Android id", getAndroidId(this)}));
         list.add(createMap(new String[]{"gsm.version.baseband", ""}));
-        list.add(createMap(new String[]{"Phone Number", getPhoneNum(this)}));
-        list.add(createMap(new String[]{"IMEI", getIMEI(this)}));
-        list.add(createMap(new String[]{"Sim Subscriber id", getSubscriberId(this)}));
-        list.add(createMap(new String[]{"Sim Operator", getSimOperator(this)}));
-        list.add(createMap(new String[]{"SIM 国家代码", getSimCountryIso(this)}));
-        list.add(createMap(new String[]{"Sim Operator Name", getSimOperatorName(this)}));
-        list.add(createMap(new String[]{"Sim Serial Number", getSimSerialNumber(this)}));
-        list.add(createMap(new String[]{"Sim Status", String.valueOf(getSimState(this))}));
+        list.add(createMap(new String[]{"Phone Number", message.phoneNumber}));
+        list.add(createMap(new String[]{"IMEI", message.imei}));
+        list.add(createMap(new String[]{"Sim Subscriber id", message.subscriberId}));
+        list.add(createMap(new String[]{"Sim Operator", message.simOperator}));
+        list.add(createMap(new String[]{"SIM 国家代码", message.simCountryIso}));
+        list.add(createMap(new String[]{"Sim Operator Name", message.simOperatorName}));
+        list.add(createMap(new String[]{"Sim Serial Number", message.simSerialNumber}));
+        list.add(createMap(new String[]{"Sim Status", message.simState}));
         list.add(createMap(new String[]{"网络类型", getNetworkState(this)}));
         list.add(createMap(new String[]{"Wifi Name", ""}));
         list.add(createMap(new String[]{"Wifi BSSID", ""}));
@@ -138,23 +171,6 @@ public class MainActivity extends AppCompatActivity {
         TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
         int networkType = telephonyManager.getNetworkType();
         switch (networkType) {
-            /*
-             GPRS : 2G(2.5) General Packet Radia Service 114kbps
-             EDGE : 2G(2.75G) Enhanced Data Rate for GSM Evolution 384kbps
-             UMTS : 3G WCDMA 联通3G Universal Mobile Telecommunication System 完整的3G移动通信技术标准
-             CDMA : 2G 电信 Code Division Multiple Access 码分多址
-             EVDO_0 : 3G (EVDO 全程 CDMA2000 1xEV-DO) Evolution - Data Only (Data Optimized) 153.6kps - 2.4mbps 属于3G
-             EVDO_A : 3G 1.8mbps - 3.1mbps 属于3G过渡，3.5G
-             1xRTT : 2G CDMA2000 1xRTT (RTT - 无线电传输技术) 144kbps 2G的过渡,
-             HSDPA : 3.5G 高速下行分组接入 3.5G WCDMA High Speed Downlink Packet Access 14.4mbps
-             HSUPA : 3.5G High Speed Uplink Packet Access 高速上行链路分组接入 1.4 - 5.8 mbps
-             HSPA : 3G (分HSDPA,HSUPA) High Speed Packet Access
-             IDEN : 2G Integrated Dispatch Enhanced Networks 集成数字增强型网络 （属于2G，来自维基百科）
-             EVDO_B : 3G EV-DO Rev.B 14.7Mbps 下行 3.5G
-             LTE : 4G Long Term Evolution FDD-LTE 和 TDD-LTE , 3G过渡，升级版 LTE Advanced 才是4G
-             EHRPD : 3G CDMA2000向LTE 4G的中间产物 Evolved High Rate Packet Data HRPD的升级
-             HSPAP : 3G HSPAP 比 HSDPA 快些
-             */
             // 2G网络
             case TelephonyManager.NETWORK_TYPE_GPRS:
             case TelephonyManager.NETWORK_TYPE_CDMA:
@@ -217,72 +233,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @SuppressLint("MissingPermission")
-    public static String getIMEI(Context context) {
+    public static PhoneMessage getPhoneMessage(Context context) {
         TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        PhoneMessage message = new PhoneMessage();
+        message.phoneNumber = manager.getLine1Number();
+        message.imei = manager.getDeviceId();
+        message.subscriberId = manager.getSubscriberId();
+        message.simOperator = manager.getSimOperator();
+        message.simCountryIso = manager.getSimCountryIso();
+        message.simOperatorName = manager.getSimOperatorName();
+        message.simSerialNumber = manager.getSimSerialNumber();
+        message.simState = String.valueOf(manager.getSimState());
+        return message;
+    }
+
+
+    /**
+     * 通过网络接口取
+     * 获取wifiMac地址
+     *
+     * @return
+     */
+    private static String getNewMac() {
         try {
-            Method method = manager.getClass().getMethod("getImei", int.class);
-            String imei1 = (String) method.invoke(manager, 0);
-            String imei2 = (String) method.invoke(manager, 1);
-            if(TextUtils.isEmpty(imei2)){
-                return imei1;
-            }
-            if(!TextUtils.isEmpty(imei1)){
-                //因为手机卡插在不同位置，获取到的imei1和imei2值会交换，所以取它们的最小值,保证拿到的imei都是同一个
-                String imei = "";
-                if(imei1.compareTo(imei2) <= 0){
-                    imei = imei1;
-                }else{
-                    imei = imei2;
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return null;
                 }
-                return imei;
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02X:", b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return manager.getDeviceId();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        return "";
-    }
-
-    @SuppressLint("MissingPermission")
-    public static String getPhoneNum(Context context) {
-        TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        return manager.getLine1Number();//获取本机号码
-    }
-
-    @SuppressLint("MissingPermission")
-    public static String getSubscriberId(Context context) {
-        TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        return manager.getSubscriberId();//获取本机号码
-    }
-
-    @SuppressLint("MissingPermission")
-    public static String getSimOperator(Context context) {
-        TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        return manager.getSimOperator();//获取本机号码
-    }
-
-    @SuppressLint("MissingPermission")
-    public static String getSimCountryIso(Context context) {
-        TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        return manager.getSimCountryIso();//获取本机号码
-    }
-
-    @SuppressLint("MissingPermission")
-    public static String getSimOperatorName(Context context) {
-        TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        return manager.getSimOperatorName();//获取本机号码
-    }
-
-    @SuppressLint("MissingPermission")
-    public static String getSimSerialNumber(Context context) {
-        TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        return manager.getSimSerialNumber();//获取本机号码
-    }
-
-    @SuppressLint("MissingPermission")
-    public static int getSimState(Context context) {
-        TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        return manager.getSimState();//获取本机号码
+        return null;
     }
 
 }
